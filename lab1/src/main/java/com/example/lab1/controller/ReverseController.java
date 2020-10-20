a9cf5f339ff7b68d4c65f25ca45f8c6a70f6bebd
DGromek <dawidgromek.1997@gmail.com>
2020-10-20 14:44:13 +0200

    Added GET mapping for String reversing.

--- lab1/src/main/java/com/example/lab1/controller/ReverseController.java
+++ lab1/src/main/java/com/example/lab1/controller/ReverseController.java
@@ -1,9 +1,19 @@
 package com.example.lab1.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class ReverseController {
+
+    @GetMapping("/reverseString")
+    public ResponseEntity<String> reverseString(@RequestParam String toReverse) {
+        String reversed = new StringBuilder(toReverse).reverse().toString();
+        return new ResponseEntity<>(reversed, HttpStatus.OK);
+    }
 
 }
