f0106c89f596777bb648d74dee82868fbd4a1e96
DGromek <dawidgromek.1997@gmail.com>
2020-11-03 14:25:11 +0100

    Project init.

---
+++ lab2/src/test/java/pl/politechnika/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.politechnika.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
