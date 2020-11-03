ca45fd4dcd8812c858c8cfb45a47fd9772464111
DGromek <dawidgromek.1997@gmail.com>
2020-11-03 14:56:34 +0100

    Added function responsible for string analysing.

--- lab2/src/main/java/pl/politechnika/lab2/model/StringAnalyse.java
+++ lab2/src/main/java/pl/politechnika/lab2/model/StringAnalyse.java
@@ -9,4 +9,25 @@ public class StringAnalyse {
 
     public StringAnalyse() {
     }
+
+    public static StringAnalyse analyseString(String string) {
+        StringAnalyse result = new StringAnalyse();
+        char[] stringIntoChars = string.toCharArray();
+
+        for (char iter : stringIntoChars) {
+            if (iter >= 'A' && iter <= 'Z') {
+                result.uppercaseLettersCount++;
+            }
+            else if (iter >= 'a' && iter <= 'z') {
+                result.lowercaseLettersCount++;
+            }
+            else if (iter >= '0' && iter <= '9') {
+                result.numbersCount++;
+            }
+            else {
+                result.specialCharactersCount++;
+            }
+        }
+        return result;
+    }
 }
