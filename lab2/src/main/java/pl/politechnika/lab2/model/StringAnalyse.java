f30a058cbbfb5fce311eff5eef6cc098b72a1c32
DGromek <dawidgromek.1997@gmail.com>
2020-11-03 14:47:09 +0100

    Added model representing results of string analyse.

---
+++ lab2/src/main/java/pl/politechnika/lab2/model/StringAnalyse.java
@@ -0,0 +1,12 @@
+package pl.politechnika.lab2.model;
+
+public class StringAnalyse {
+
+    public int uppercaseLettersCount;
+    public int lowercaseLettersCount;
+    public int numbersCount;
+    public int specialCharactersCount;
+
+    public StringAnalyse() {
+    }
+}
