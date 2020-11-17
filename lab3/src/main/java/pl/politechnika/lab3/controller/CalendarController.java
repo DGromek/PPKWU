8f5f3a8541388815eea4280d2865353b77c1407e
DGromek <dawidgromek.1997@gmail.com>
2020-11-17 15:26:22 +0100

    Added input validation.

--- lab3/src/main/java/pl/politechnika/lab3/controller/CalendarController.java
+++ lab3/src/main/java/pl/politechnika/lab3/controller/CalendarController.java
@@ -6,11 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class CalendarController {
 
     @GetMapping("/weeiaCalendar")
-    public ResponseEntity<Integer> getICSCalendar(@RequestParam int month) {
-        return new ResponseEntity<>(month, HttpStatus.OK);
+    public ResponseEntity<String> getICSCalendar(@RequestParam int month) {
+        if (!isCorrectMonth(month)) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        String formatted = String.format("%02d", month);
+        return new ResponseEntity<>(formatted, HttpStatus.OK);
+    }
+
+    private boolean isCorrectMonth(int month) {
+        return month > 0 && month <= 12;
     }
 }
