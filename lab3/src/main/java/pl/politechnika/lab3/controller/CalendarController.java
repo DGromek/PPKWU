97ddca8553d69d41d0b7aa5e064421c64bf1b2cf
DGromek <dawidgromek.1997@gmail.com>
2020-12-01 15:30:15 +0100

    Refactored code.

--- lab3/src/main/java/pl/politechnika/lab3/controller/CalendarController.java
+++ lab3/src/main/java/pl/politechnika/lab3/controller/CalendarController.java
@@ -10,7 +10,6 @@ import net.fortuna.ical4j.util.RandomUidGenerator;
 import net.fortuna.ical4j.util.UidGenerator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,22 +26,21 @@ import java.util.List;
 public class CalendarController {
 
     @GetMapping("/weeiaCalendar")
-    public ResponseEntity<String> getICSCalendar(@RequestParam int month) throws IOException, ParseException {
+    public ResponseEntity<String> getICSCalendar(@RequestParam int month) throws IOException {
         if (!isCorrectMonth(month)) {
             return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
-        String formatted = String.format("%02d", month);
-        getWEEIACalendarForGivenMonth(month);
-        return new ResponseEntity<>(formatted, HttpStatus.OK);
+        int currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+        getWEEIACalendarForGivenMonth(currentYear, month);
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 
     private boolean isCorrectMonth(int month) {
         return month > 0 && month <= 12;
     }
 
-    private List<String> getListOfEvents(String monthInCorrectFormat) throws IOException {
-        int currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
-        Document doc = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + currentYear + "&miesiac=" + monthInCorrectFormat).get();
+    private List<String> getListOfEvents(int year, int month) throws IOException {
+        Document doc = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + month).get();
         Elements events = doc.getElementsByClass("active");
         List<String> result = new ArrayList<>();
         for (int i = 0; i < events.size(); i++) {
@@ -54,20 +51,19 @@ public class CalendarController {
         return result;
     }
 
-    private Calendar getWEEIACalendarForGivenMonth(int month) throws IOException, ParseException {
+    private Calendar getWEEIACalendarForGivenMonth(int year, int month) throws IOException {
         Calendar calendar = new Calendar();
         calendar.getProperties().add(new ProdId("WEEIACalendar"));
         calendar.getProperties().add(Version.VERSION_2_0);
         calendar.getProperties().add(CalScale.GREGORIAN);
 
-        int currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
-        String monthInCorrectFormat = String.format("%02d", month);
-        String dateToFill = currentYear + monthInCorrectFormat;
-        List<String> events = getListOfEvents(String.format("%02d", month));
+        List<String> events = getListOfEvents(year, month);
 
         for (String event : events) {
             String[] separateDayAndEventName = event.split(" ", 2);
-            VEvent calendarEvent = new VEvent(new Date(dateToFill + String.format("%02d", Integer.parseInt(separateDayAndEventName[0]))), separateDayAndEventName[1]);
+            int dayParsed = Integer.parseInt(separateDayAndEventName[0]);
+            java.util.Date date = new java.util.Date(year, month, dayParsed);
+            VEvent calendarEvent = new VEvent(new Date(date), separateDayAndEventName[1]);
             UidGenerator ug = new RandomUidGenerator();
             calendarEvent.getProperties().add(ug.generateUid());
             calendar.getComponents().add(calendarEvent);
