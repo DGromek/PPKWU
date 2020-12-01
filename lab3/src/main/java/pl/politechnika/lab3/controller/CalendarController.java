6fb2d14cdc06db68ca1450290cac385c5fd76be5
DGromek <dawidgromek.1997@gmail.com>
2020-12-01 15:24:07 +0100

    Early version of generating ics. Not optimal and not tested yet.

--- lab3/src/main/java/pl/politechnika/lab3/controller/CalendarController.java
+++ lab3/src/main/java/pl/politechnika/lab3/controller/CalendarController.java
@@ -1,5 +1,13 @@
 package pl.politechnika.lab3.controller;
 
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Version;
+import net.fortuna.ical4j.util.RandomUidGenerator;
+import net.fortuna.ical4j.util.UidGenerator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +28,12 @@ import java.util.List;
 public class CalendarController {
 
     @GetMapping("/weeiaCalendar")
-    public ResponseEntity<String> getICSCalendar(@RequestParam int month) throws IOException {
+    public ResponseEntity<String> getICSCalendar(@RequestParam int month) throws IOException, ParseException {
         if (!isCorrectMonth(month)) {
             return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
         String formatted = String.format("%02d", month);
-        getListOfEvents(formatted);
+        getWEEIACalendarForGivenMonth(month);
         return new ResponseEntity<>(formatted, HttpStatus.OK);
     }
 
@@ -43,5 +52,26 @@ public class CalendarController {
             }
         }
         return result;
+    }
+
+    private Calendar getWEEIACalendarForGivenMonth(int month) throws IOException, ParseException {
+        Calendar calendar = new Calendar();
+        calendar.getProperties().add(new ProdId("WEEIACalendar"));
+        calendar.getProperties().add(Version.VERSION_2_0);
+        calendar.getProperties().add(CalScale.GREGORIAN);
+
+        int currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+        String monthInCorrectFormat = String.format("%02d", month);
+        String dateToFill = currentYear + monthInCorrectFormat;
+        List<String> events = getListOfEvents(String.format("%02d", month));
+
+        for (String event : events) {
+            String[] separateDayAndEventName = event.split(" ", 2);
+            VEvent calendarEvent = new VEvent(new Date(dateToFill + String.format("%02d", Integer.parseInt(separateDayAndEventName[0]))), separateDayAndEventName[1]);
+            UidGenerator ug = new RandomUidGenerator();
+            calendarEvent.getProperties().add(ug.generateUid());
+            calendar.getComponents().add(calendarEvent);
+        }
+        return calendar;
     }
 }
