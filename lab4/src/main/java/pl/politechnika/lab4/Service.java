3f83fc67849772710f411f6134d200df837ab249
DGromek <dawidgromek.1997@gmail.com>
2020-12-15 14:54:15 +0100

    Added Service model .

---
+++ lab4/src/main/java/pl/politechnika/lab4/Service.java
@@ -0,0 +1,16 @@
+package pl.politechnika.lab4;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Service {
+    private String name;
+    private String description;
+    private String address;
+    private String phone;
+    private String email;
+}
